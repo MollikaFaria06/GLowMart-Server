ddd5a0b551215f6a3ce016e9c71480fd95d6d14f
MollikaFaria06 <fariamk007@gmail.com>
2026-01-20 17:20:06 +0600

    Added vercel.json

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://glowmart-client.vercel.app"]
+}));
 app.use(express.json());
 
 // MongoDB URI
