0c3a999fa5ece9cb35be2489c4af10820b09a6e5
MollikaFaria06 <fariamk007@gmail.com>
2026-01-20 17:51:08 +0600

    Added vercel.json

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB URI
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2xgatkm.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.2xgatkm.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
